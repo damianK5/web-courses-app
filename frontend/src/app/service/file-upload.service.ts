558551b1aa6aaea243d6cb4aaebdaace15572928
Jakub Miarka <jakvbmiarka@gmail.com>
2025-06-04 16:47:49 +0200

    basic upload implemeented

--- frontend/src/app/service/file-upload.service.ts
+++ frontend/src/app/service/file-upload.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
+private apiServerUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public uploadAsset(file: File, courseID: number): Observable<boolean>
+  {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<boolean>(`/files/${courseID}/asset`, formData)
+  }
+
+  public uploadHomework(file:File, courseID: number): Observable<boolean>
+
+{
+  const formData = new FormData();
+  formData.append('file', file);
+  return this.http.post<boolean>(`/files/${courseID}/homework`, formData)
 }
+
+public uploadAdmission(file: File, courseID: number, userID: number, homeworkID: number): Observable<boolean>
+{
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('userid', userID.toString());
+  return this.http.post<boolean>(`/files/${courseID}/${homeworkID}`, formData)
+}
+}
+
+  
